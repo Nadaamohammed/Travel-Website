ae44e07ef15ec827903dcac47a018026ff0b2b4a
Nada Mohamed <125500844+Nadaamohammed@users.noreply.github.com>
2025-12-18 13:50:17 -0800

    DomainLayer

---
+++ DomainLayer/RepositoryInterface/Booking&Transaction/IBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using DomainLayer.Models.Booking_Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.RepositoryInterface.Booking_Transaction
+{
+    public interface IBookingRepository:IGenericRepository<Booking,int>
+    {
+    }
+}
