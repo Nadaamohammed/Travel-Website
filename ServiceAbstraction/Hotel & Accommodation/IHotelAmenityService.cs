552877a508672496c0a7b47ea3910c8edea391fb
Nada Mohamed <125500844+Nadaamohammed@users.noreply.github.com>
2025-12-18 13:53:20 -0800

    Abstrcation

---
+++ ServiceAbstraction/Hotel & Accommodation/IHotelAmenityService.cs
@@ -0,0 +1,54 @@
+﻿//using Shared.Dto_s.Hotel___Accommodation;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ServiceAbstraction.Hotel___Accommodation
+//{
+//    public interface IHotelAmenityService
+//    {
+//            //Task<IEnumerable<HotelAmenityDto>> GetAllAsync();
+//            Task<HotelAmenityDto> AddAsync(HotelAmenityDto dto);
+//            Task<bool> DeleteAsync(int hotelId, int amenityId);
+//            Task<IEnumerable<HotelAmenityDto>> GetByHotelAsync(int hotelId);
+
+//    }
+//    }
+using Shared.Dto_s.Hotel___Accommodation;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceAbstraction.Hotel___Accommodation
+{
+    public interface IHotelAmenityService
+    {
+        /// <summary>
+        /// Get all hotel-amenity relations.
+        /// </summary>
+        Task<IEnumerable<HotelAmenityDto>> GetAllAsync();
+
+        /// <summary>
+        /// Get a hotel-amenity by hotelId and amenityId.
+        /// </summary>
+        Task<HotelAmenityDto> GetByIdAsync(int hotelId, int amenityId);
+
+        /// <summary>
+        /// Get all amenities for a specific hotel.
+        /// </summary>
+        Task<IEnumerable<HotelAmenityDto>> GetByHotelAsync(int hotelId);
+
+        /// <summary>
+        /// Add a new hotel-amenity relation.
+        /// </summary>
+        Task<HotelAmenityDto> AddAsync(HotelAmenityDto dto);
+
+        /// <summary>
+        /// Delete a hotel-amenity relation.
+        /// </summary>
+        Task<bool> DeleteAsync(int hotelId, int amenityId);
+    }
+}
+
+
---
+++ ServiceAbstraction/IPaymentService.cs
@@ -0,0 +1,19 @@
+﻿using DomainLayer.Models.Booking_Transaction;
+using DomainLayer.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAbstraction
+{
+    public interface IPaymentService
+    {
+
+        Task<Booking> CreateOrUpdatePaymentIntent(int bookingId);
+        Task<bool> UpdatePaymentStatus(string paymentIntentId, bool isPaid);
+        Task<bool> ConfirmPayment( string paymentIntentId);
+        Task HandleSuccessfulPayment(string paymentIntentId);
+    }
+}
